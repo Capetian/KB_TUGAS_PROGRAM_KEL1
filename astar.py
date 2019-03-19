08895a8c571bd61c84a74d00bfcfad84cb6563bd
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2019-03-19 16:22:54 +0700

    fix astar pake heuristic 2

--- astar.py
+++ astar.py
@@ -20,11 +20,18 @@ queue_popped = []
 queue_res = deque([])
 end_state = None
 
-queue[0].printBoard()
+time = 0
 maxDepth = 0
 while len(queue):
+    time += 1
     now = heapq.heappop(queue)
 
+    # Untuk Syarat Page 27 solution.pdf
+    if now.isSame(goal_state):
+        end_state = now
+        break
+
+     # Untuk Syarat Page 10 solution.pdf
     isSameBefore = False
 
     for state in queue_popped:
@@ -42,33 +49,49 @@ while len(queue):
     leftState = now.expandLeft()
     rightState = now.expandRight()
 
+    # Untuk Syarat Page 8 solution.pdf
+    for idx, state in enumerate(queue):
+        if upState is not None and upState.isSame(state.board) and upState.__lt__(state):
+            queue[idx] = upState
+        if downState is not None and downState.isSame(state.board) and downState.__lt__(state):
+            queue[idx] = downState
+        if leftState is not None and leftState.isSame(state.board) and leftState.__lt__(state):
+            queue[idx] = leftState
+        if rightState is not None and rightState.isSame(state.board) and rightState.__lt__(state):
+            queue[idx] = rightState
 
     if upState is not None:
-        if upState.isSame(goal_state):
-            end_state = upState
-            break
+        #upState.printBoard()
+        if upState.cost > maxDepth:
+            maxDepth = upState.cost
         heapq.heappush(queue, upState)
     if downState is not None:
-        if downState.isSame(goal_state):
-            end_state = downState
-            break
+        #downState.printBoard()
+        if downState.cost > maxDepth:
+            maxDepth = downState.cost
         heapq.heappush(queue, downState)
     if rightState is not None:
-        if rightState.isSame(goal_state):
-            end_state = rightState
-            break
+        #rightState.printBoard()
+        if rightState.cost > maxDepth:
+            maxDepth = rightState.cost
         heapq.heappush(queue, rightState)
     if leftState is not None:
-        if leftState.isSame(goal_state):
-            end_state = leftState
-            break
+        #leftState.printBoard()
+        if leftState.cost > maxDepth:
+            maxDepth = leftState.cost
         heapq.heappush(queue, leftState)
 
 while end_state is not None:
     queue_res.appendleft(end_state)
     end_state = end_state.parent
 
-step = 1
+step = 0
+
+print("Jumlah Loop\t\t\t: " + str(time))
+print("Jumlah langkah yang ditelusuri\t: " + str(len(queue)) )
+print("Maximum Depth\t\t\t: " + str(maxDepth) )
+print("Jumlah langkah solusi\t\t: " + str(len(queue_res) - 1) )
+print("\nSolusi: \n")
 
 while len(queue_res):
     print("Step #" + str(step))
--- state.py
+++ state.py
@@ -1,4 +1,11 @@
+import math
+
 class State:
+    TYPE_BOARD_NOW = 0
+    TYPE_BOARD_GOAL = 1
+
+    posBoard = None
+    posGoal_Board = None
     indexNull = None
     heuristic = None
     upState = None
@@ -78,11 +85,10 @@ class State:
         return False
 
     def isSame(self, otherBoard):
-        same = True
         for idx, val in enumerate(self.board):
             if val != otherBoard[idx]:
                 return False
-        return same
+        return True
 
     def printBoard(self):
         i = 0
@@ -96,14 +102,41 @@ class State:
     def getHeuristic(self, goalBoard):
         if self.heuristic is None:
             diff = 0
-            for idx, val in enumerate(self.board):
-                if val != goalBoard[idx]:
-                    diff += 1
+            for val in self.board:
+                posNow = self.getPos(self.TYPE_BOARD_NOW, val)
+                posGoal = self.getPos(self.TYPE_BOARD_GOAL, val)
+                posNow_x = posNow % 3
+                posNow_y = math.ceil(posNow / 3)
+                posGoal_x = posGoal % 3
+                posGoal_y = math.ceil(posGoal / 3)
+                diff += abs(posNow_x - posGoal_x) + abs(posNow_y - posGoal_y)
             self.heuristic = diff
         return self.heuristic
+        # if self.heuristic is None:
+        #     diff = 0
+        #     for idx, val in enumerate(self.board):
+        #         if val != goalBoard[idx]:
+        #             diff += 1
+        #     self.heuristic = diff
+        # return self.heuristic
+
+    def getPos(self, type, number):
+        if type == self.TYPE_BOARD_NOW:
+            if self.posBoard is None:
+                self.posBoard = [None, None, None, None, None, None, None, None, None]
+                for idx, val in enumerate(self.board):
+                    self.posBoard[val] = idx
+            return self.posBoard[number]
+        elif type == self.TYPE_BOARD_GOAL:
+            if self.posGoal_Board is None:
+                self.posGoal_Board = [None, None, None, None, None, None, None, None, None]
+                for idx, val in enumerate(self.goal_board):
+                    self.posGoal_Board[val] = idx
+            return self.posGoal_Board[number]
+        return None
 
     def __lt__(self, other):
         """
         :type other: State
         """
-        return self.getHeuristic(self.goal_board) < other.getHeuristic(self.goal_board)
+        return self.cost + self.getHeuristic(self.goal_board) < other.cost + other.getHeuristic(self.goal_board)
